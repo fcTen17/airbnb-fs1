fbd90dea04112f7a4381666ed471dc6e4415397c
Fernando Carvalho <fernandocesartenorio@gmail.com>
2022-03-16 15:59:36 -0300

    properties API setup: MVC

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,33 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_073033) do
+ActiveRecord::Schema.define(version: 2022_03_16_181131) do
+
+  create_table "properties", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "city"
+    t.string "country"
+    t.string "propertiy_type"
+    t.integer "price_per_night"
+    t.integer "max_guests"
+    t.integer "bedrooms"
+    t.integer "beds"
+    t.integer "baths"
+    t.string "image_url"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_properties_on_user_id"
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.string "token"
@@ -28,5 +46,6 @@ ActiveRecord::Schema.define(version: 2020_09_22_073033) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "properties", "users"
   add_foreign_key "sessions", "users"
 end
